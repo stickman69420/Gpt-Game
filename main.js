278bd7fa642d15c8fd03ecd4415a59b608de49ce
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-07-30 09:58:39 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-
+var npm = require('crypto-js');
+    function sub() {
+      const encryptedText = CryptoJS.AES.encrypt(in.value, "wsxdecrftgtgyhujixevbn").toString();
+      console.log(encryptedText);
+    }
