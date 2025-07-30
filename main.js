c1133c3499c78f3c045f9429f68c6b4dde394b7a
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-07-30 19:29:14 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-function sub() {
+function gen() {
   const Ip = (localStorage.getItem("IP")).replaceAll(".","")
   let Iprocess = "";
-  console.log(localStorage.getItem("IP"));
   for (let i = 0; i < Ip.length-1; i+=2) {
     Iprocess += 9-(Ip[i+1])
     Iprocess += Math.floor(Math.random()*10)
     Iprocess += Ip[i]
     Iprocess += Math.floor(Math.random()*10)
   }
-  console.log(Iprocess);
+  output.innerHtml = Iprocess;
+}
+function sub(){
+  console.log("nothing")
 }
