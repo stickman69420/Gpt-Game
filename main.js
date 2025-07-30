b37d81c0164f8ee61351b0ae570b81f791d33ff7
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-07-31 09:11:43 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
+let Webs
 function gen() {
   const Ip = (localStorage.getItem("IP")).replaceAll(".","")
-  console.log(localStorage.getItem("IP"))
   let Iprocess = "";
   for (let i = 0; i < Ip.length-1; i+=2) {
     Iprocess += 9-(Ip[i+1])
@@ -10,7 +10,6 @@ function gen() {
   }
   Iprocess += Ip[Ip.length-1]
   Iprocess += Math.floor(Math.random()*10)
-  console.log(Iprocess);
   output.innerHTML = Iprocess;
 }
 function sub(){
@@ -20,7 +19,10 @@ function sub(){
     inpvp += inpv[i+2]
     inpvp += 9-inpv[i]
   }
-  inpvp += inpv[inpv.length-2]
-  inpvp = inpvp.slice(0,3)+"."+inpvp.slice(3,6)+"."+inpvp.slice(6,7)+"."+inpvp.slice(7)
-  console.log(inpvp)
+  inpvp += inpv[inpv.length-2];
+  inpvp = inpvp.slice(0,3)+"."+inpvp.slice(3,6)+"."+inpvp.slice(6,7)+"."+inpvp.slice(7);
+  WebS = new WebSocket(inpvp);
 }
+WebS.addEventListener("message", (event) => {
+  document.write(event.data);
+})
