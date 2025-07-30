f9dd449bfef5ae2fca22c74cd632cf7778b769c8
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-07-30 13:26:31 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let CryptoJS = (import("https://github.com/brix/crypto-js"));
-document.getelementbyid("out").innerHTML = fetch("https://api.ipify.org")
+document.getElementById("out").innerHTML = fetch("https://api.ipify.org")
 function sub() {
   console.log(CryptoJS.AES.encrypt(document.getelementbyid("in").value, "jsjjsjsidjusiskjsigooakwoockzopwlsoosspkgjbvkskj83"))
 }
