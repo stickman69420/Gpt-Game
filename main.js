545c0b0f246d4141b7d174ad87ebd44eea78b75a
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-07-30 17:38:10 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function sub() {
-  const Ip = localStorage.getItem("IP")
+  const Ip = (localStorage.getItem("IP")).replaceAll(".","")
   let Iprocess = "";
   console.log(Ip);
   for (let i = 0; i < Ip.length-1; i+=2) {
