834c672aa7799f5d4eea370e182b5587249c3478
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-07-30 17:09:14 +1000

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ function sub() {
   let Iprocess = Ip;
   console.log(Iprocess);
   for (let i = 0; i < Ip.length; i+=2) {
-    Iprocess = Iprocess[0,i]+Ip[i];+Iprocess[i+1,-1]
-    Iprocess = Iprocess[0,i+1]+Ip[i+1];+Iprocess[i+2,-1]
+    Iprocess = Iprocess[0,i]+Ip[i+1];+Iprocess[i+1,-1]
+    Iprocess = Iprocess[0,i+1]+Ip[i];+Iprocess[i+2,-1]
   }
   console.log(Iprocess);
 }
