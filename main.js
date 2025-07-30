74d40e448e5b41ca61ed56bee2e18ddac6e66eae
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-07-30 15:44:58 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function sub() {
   const Ip = localstorage.getItem("IP")
-  let Iprocess = Ip
-  console.log(Ip)
+  let Iprocess = Ip;
+  console.log(Iprocess);
   for (let i = 0; i < Ip.length; i+=2) {
-    Iprocess[i+1] = Ip[i]
-    Iprocess[i]= Ip[i+1]
+    Iprocess[i+1] = Ip[i];
+    Iprocess[i]= Ip[i+1];
   }
-  console.log(Iprocess)
+  console.log(Iprocess);
 }
