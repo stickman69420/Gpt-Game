074004030a7b8891aff2c76f05571146771c0682
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-07-30 13:02:02 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let CryptoJS = (import("https://github.com/brix/crypto-js"));
+setInterval(loop,100);
 function sub() {
   console.log(CryptoJS.AES.encrypt(document.getelementbyid("in").value, "jsjjsjsidjusiskjsigooakwoockzopwlsoosspkgjbvkskj83"))
+}
+function loop() {
+  console.log(CryptoJS);
 }
