333cf5f18fd0682fc068e68247e70b35f41811a0
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-07-31 08:37:06 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function gen() {
   const Ip = (localStorage.getItem("IP")).replaceAll(".","")
+  console.log(localStorage.getItem("IP"))
   let Iprocess = "";
   for (let i = 0; i < Ip.length-1; i+=2) {
     Iprocess += 9-(Ip[i+1])
@@ -11,5 +12,10 @@ function gen() {
   output.innerHTML = Iprocess;
 }
 function sub(){
-  console.log("nothing")
+  let inpv = input.value
+  let inpvp = ""
+  for (let i = 0; i < inpv.length-1; i+=4) {
+    inpvp += 9-inpv[i]
+    inpvp += inpv[i]
+  }
 }
