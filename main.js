452a5465936575ee27ae132ba564f630f83c16ae
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-07-30 17:44:02 +1000

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function sub() {
   console.log(Ip);
   for (let i = 0; i < Ip.length-1; i+=2) {
     Iprocess += 9-(Ip[i+1])
+    Iprocess += Math.floor(Math.random()*10)
     Iprocess += Ip[i]
   }
   console.log(Iprocess);
