84fb5bd0d30e35ad70ca8df3f98409df2b1fe2c0
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-07-30 14:40:58 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 //let CryptoJS = (import("https://github.com/brix/crypto-js"));
-async function GetIp(){
+/*async function GetIp(){
   return await(fetch("https://api.ipify.org?format=json"))
 }
 GetIp().then(ip => {
   console.log(ip)
   document.getElementById("out").innerHTML = ip;
-});
+});*/
+try (java.util.Scanner s = new java.util.Scanner(new java.net.URL("https://api.ipify.org").openStream(), "UTF-8").useDelimiter("\\A")) {
+    document.getElementById("out").innerHTML = ("My current IP address is " + s.next());
+} catch (java.io.IOException e) {
+    e.printStackTrace();
+}
 /*function sub() {
   console.log(CryptoJS.AES.encrypt(document.getelementbyid("in").value, "jsjjsjsidjusiskjsigooakwoockzopwlsoosspkgjbvkskj83"))
 }*/
