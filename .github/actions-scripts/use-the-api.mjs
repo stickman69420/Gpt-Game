f0a3d6579d90860d1ce2f02ec65429ba3799259e
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2025-08-11 10:30:48 +1000

    Update use-the-api.mjs

--- .github/actions-scripts/use-the-api.mjs
+++ .github/actions-scripts/use-the-api.mjs
@@ -6,8 +6,8 @@ const octokit = new Octokit({
 
 try {
   const result = await octokit.request("GET /repos/{owner}/{repo}/issues", {
-      owner: "octocat",
-      repo: "Spoon-Knife",
+      owner: "stickman69420",
+      repo: "Gpt-Game",
     });
 
   const titleAndAuthor = result.data.map(issue => {title: issue.title, authorID: issue.user.id})
